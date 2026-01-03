f152fb38c7259c744460e3412961bed98be72920
koro <koro@korostudio.cn>
2026-01-03 15:04:14 +0800

    add foxdongle33

---
+++ src/boards/foxdongle33_nrf52833/board.h
@@ -0,0 +1,69 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 Ha Thach for Adafruit Industries
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef _FOXDONGLE33
+#define _FOXDONGLE33
+
+#define _PINNUM(port, pin)    ((port)*32 + (pin))
+
+
+/*------------------------------------------------------------------*/
+/* LED
+ *------------------------------------------------------------------*/
+#define LEDS_NUMBER           2
+#define LED_PRIMARY_PIN       _PINNUM(0, 2)// blue
+#define LED_SECONDARY_PIN     _PINNUM(0, 3)// red
+#define LED_STATE_ON          1
+
+#define NEOPIXELS_NUMBER      0
+
+/*------------------------------------------------------------------*/
+/* BUTTON
+ *------------------------------------------------------------------*/
+#define BUTTONS_NUMBER        2
+#define BUTTON_1              _PINNUM(0, 18)
+// Button 2 is from FoxSmol expansion board.
+#define BUTTON_2              _PINNUM(0, 4)
+#define BUTTON_PULL           NRF_GPIO_PIN_PULLUP
+
+//--------------------------------------------------------------------+
+// BLE OTA
+//--------------------------------------------------------------------+
+#define BLEDIS_MANUFACTURER   "FoxApplication"
+#define BLEDIS_MODEL          "FoxDobgle33"
+
+//--------------------------------------------------------------------+
+// USB
+//--------------------------------------------------------------------+
+#define USB_DESC_VID           0x2fe3
+#define USB_DESC_UF2_PID       0x0032
+#define USB_DESC_CDC_ONLY_PID  0x0032
+
+//------------- UF2 -------------//
+#define UF2_PRODUCT_NAME   "FoxApplication FoxDobgle33 V1"
+#define UF2_VOLUME_LABEL   "FOX-BOOT-Dongle33"
+#define UF2_BOARD_ID       "FoxDobgle33-v1"
+#define UF2_INDEX_URL      "https://www.foxapplication.com/"
+
+#endif // _FOXDONGLE33
