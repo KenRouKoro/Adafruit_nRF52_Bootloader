a664464081c0e72351995dff9ca4e6f648a3cf42
koro <koro@korostudio.cn>
2026-01-20 18:03:02 +0800

    Change FoxSnackLite to LDO Mode

--- src/boards/foxsnacklite_nrf52840/board.h
+++ src/boards/foxsnacklite_nrf52840/board.h
@@ -27,8 +27,8 @@
 
 #define _PINNUM(port, pin)    ((port)*32 + (pin))
 
-#define ENABLE_DCDC_1 1
-#define ENABLE0_DCDC_0 0
+#define ENABLE_DCDC_1 0
+#define ENABLE_DCDC_0 0
 
 #define UICR_REGOUT0_VALUE UICR_REGOUT0_VOUT_2V7
 
